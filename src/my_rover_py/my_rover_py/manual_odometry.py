652ad6af6f2a37e338694d5247ca5be45c38bf3a
Giovanni <41193446+GioTKD@users.noreply.github.com>
2024-09-13 19:34:24 +0200

    Add files via upload

--- src/my_rover_py/my_rover_py/manual_odometry.py
+++ src/my_rover_py/my_rover_py/manual_odometry.py
@@ -11,14 +11,12 @@ class ManualOdometry(Node):
         # Open files to save the position and tf data
         self.position_file = open('position_data.txt', 'w')
         self.tf_file = open('tf_data.txt', 'w')
-        self.tf_static_file = open('tf_static_data.txt', 'w')
 
         # Subscribe to /odom topic
         self.sub = self.create_subscription(Odometry, '/odom', self.odom_callback, 10)
 
-        # Subscriptions for /tf and /tf_static
+        # Subscriptions for /tf
         self.tf_sub = self.create_subscription(TFMessage, '/tf', self.tf_callback, 10)
-        self.tf_static_sub = self.create_subscription(TFMessage, '/tf_static', self.tf_static_callback, 10)
 
     def odom_callback(self, msg):
         """Callback to handle /odom data"""
@@ -31,16 +29,10 @@ class ManualOdometry(Node):
         for transform in msg.transforms:
             self.tf_file.write(f"{transform.header.stamp.sec}.{transform.header.stamp.nanosec}: {transform.child_frame_id}, {transform.transform.translation}, {transform.transform.rotation}\n")
 
-    def tf_static_callback(self, msg):
-        """Callback to save /tf_static data to a file"""
-        for transform in msg.transforms:
-            self.tf_static_file.write(f"{transform.header.stamp.sec}.{transform.header.stamp.nanosec}: {transform.child_frame_id}, {transform.transform.translation}, {transform.transform.rotation}\n")
-
     def destroy_node(self):
         # Close the files when the node is destroyed
         self.position_file.close()
         self.tf_file.close()
-        self.tf_static_file.close()
         super().destroy_node()
 
 def main(args=None):
